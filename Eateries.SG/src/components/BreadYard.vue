757b287b7768b4e83938d8c27f376d973c2f7b84
Vinodjayakumar124 <60294470+Vinodjayakumar124@users.noreply.github.com>
2020-11-01 11:15:03 +0800

    removal of banners on Menu pages

--- Eateries.SG/src/components/BreadYard.vue
+++ Eateries.SG/src/components/BreadYard.vue
@@ -1,13 +1,5 @@
 <template>
   <body>
-    <div id="Banner" class="banner">
-      <div class="banner-wrap">
-        <div class="notice-text">
-          Check out our promotions and don't forget to invite yourfriends for
-          more perks!
-        </div>
-      </div>
-    </div>
     <div
       data-collapse="medium"
       data-animation="default"
