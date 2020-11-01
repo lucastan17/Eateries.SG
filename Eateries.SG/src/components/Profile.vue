9da480d8dffd4399c4c6a43d01fbb4f8e1fa2dc7
Vinodjayakumar124 <60294470+Vinodjayakumar124@users.noreply.github.com>
2020-11-01 11:59:28 +0800

    Editing the top router links for all pages

--- Eateries.SG/src/components/Profile.vue
+++ Eateries.SG/src/components/Profile.vue
@@ -6,12 +6,12 @@
                 <div class="logo-text">EATERIES.SG</div>
             </div>
             <div class="navigation-right">
-                <router-link to="/" exact>Home</router-link>
+                <router-link class = "link" to="/" exact>Home</router-link>
                 <router-link class = "current"  to="/profile" exact>Profile</router-link>
-                <router-link to="/history" exact>History</router-link>
-                <router-link to="/partnereateries" exact>Partner Eateries</router-link>
-                <router-link to="/currentbookings" exact>Current Bookings</router-link>
-                <router-link to="/declaration" exact>Declaration</router-link>
+                <router-link class = "link" to="/history" exact>History</router-link>
+                <router-link class = "link" to="/partnereateries" exact>Partner Eateries</router-link>
+                <router-link class = "link" to="/currentbookings" exact>Current Bookings</router-link>
+                <router-link class = "link" to="/declaration" exact>Declaration</router-link>
             </div>
         </div>
     </div>
