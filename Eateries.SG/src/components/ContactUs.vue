4bb95b30aa079f75b48e23d2a2e6d130a0842087
hengjeejoo <jeejoo2@gmail.com>
2020-11-10 00:14:44 +0800

    Simple Contact Us pages and icon added

--- Eateries.SG/src/components/ContactUs.vue
+++ Eateries.SG/src/components/ContactUs.vue
@@ -28,7 +28,17 @@
     <div class="container">
         <div class="w-layout-grid content-grid">
         <div class="content-block">
-          
+          <div class="navigation-left">
+              <img src="..\assets\ContactUs.png" loading="lazy" width="83" alt="" />
+              <div class="logo-text"><h2>Got any enquiries or feedback? Contact Us!</h2></div>
+          </div>
+          <div class="content-section">
+              <h5>Office Number: 62233456</h5><br>
+              <h5>Email: eateriessg@email.com</h5><br>
+              <h5>Address: Eateries.SG, 20 Positive Road, Singapore 123456</h5>
+          </div>
+          <p>Feedbacks and suggestions are what gives us the motivation to improve our services! Here at Eateries.SG, we take every feedback and suggestions seriously</p>
+          <p>Should you have any feedback to give us, do not hesitate to contact us!</p>
         </div>
         </div>
     </div>
