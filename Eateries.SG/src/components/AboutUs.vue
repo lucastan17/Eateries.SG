5c81f8333fb8a8822560bb7dd10a12a833f410e5
hengjeejoo <jeejoo2@gmail.com>
2020-11-11 01:21:44 +0800

    Fixed Bugged for About Us page
    About Us navigation tab shows the other components only when signed in, else it shows only login and sign up component

--- Eateries.SG/src/components/AboutUs.vue
+++ Eateries.SG/src/components/AboutUs.vue
@@ -5,13 +5,18 @@
             <div class="navigation-left"><img src="..\assets\EateriesSG.svg" loading="lazy" width="83" height="auto">
                 <div class="logo-text">EATERIES.SG</div>
             </div>
-            <div class="navigation-right">
+            <div class="navigation-right" v-if="signedIn()">
                 <router-link class = "link" to="/home" exact>Home</router-link>
                 <router-link class = "link" to="/profile" exact>Profile</router-link>
                 <router-link class = "link" to="/history" exact>History</router-link>
                 <router-link class = "link" to="/partnereateries" exact>Partner Eateries</router-link>
                 <router-link class = "link" to="/currentbookings" exact>Current Bookings</router-link>
                 <router-link class = "link" to="/declaration" exact>Declaration</router-link>
+                <button class="SObutton" @click ="signOut()"><img src="..\assets\logout.svg" style="width:24px; height:22px; float:left">Logout</button>
+            </div>
+            <div class="navigation-right" v-else>
+                <router-link class ="current" to="/" exact>Log-In</router-link>
+                <router-link class = "link" to="/signup" exact>Sign-Up</router-link>
             </div>
         </div>
     </div>
@@ -32,7 +37,7 @@
           <p>In this time of COVID-19, restaurants have been hit the hardest due to restrictions implemented in light of the pandemic. Join us in  your favourite eateries to continue delivering quality food and service!</p>
         </div>
         <div class="image-blsupportingock"><img src="../assets/sg_food.png" alt="">
-            <router-link to="/partnereateries" tag="button" class="button">Check out our partner eateries now!</router-link>
+            <!--<router-link to="/partnereateries" tag="button" class="button">Check out our partner eateries now!</router-link>-->
         </div>
         </div>
     </div>
@@ -42,7 +47,24 @@
 </template>
 
 <script>
-    
+    import fb from 'firebase'
+    export default{
+        methods:{
+            signOut(){
+                fb.auth().signOut().then(() => {
+                    this.$router.replace('/')
+                })
+                .catch(err =>{
+                    this.error = err.message;
+                })
+            },
+            
+            signedIn(){
+                console.log(fb.auth().currentUser)
+                return fb.auth().currentUser != null
+            }
+        }
+    }
 </script>
 
 <style>
@@ -66,6 +88,17 @@
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.SObutton {
+    margin-left:10px;
+    background-color:whitesmoke;
+    font-size: 18px;
+    border-radius: 12px;
+    border: 2px solid #990000;
+    width: 120px;
+    font-family: sans-serif;
+    padding-right: 15px;
 }
 </style>
 
