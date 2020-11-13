d95bde9f013d75d77eb55789ef410680fa14fdc5
Vinodjayakumar124 <60294470+Vinodjayakumar124@users.noreply.github.com>
2020-11-14 00:11:36 +0800

    Styling for the Sign-up page with new content

--- Eateries.SG/src/components/Login.vue
+++ Eateries.SG/src/components/Login.vue
@@ -29,8 +29,8 @@
         <h4>Sign in with your email and password</h4>
         <br>
         <div class ="content-section">
-            <h5><a style ="padding:20px;">Email:</a> <input type="text" v-model="email" placeholder="Email"></h5><p></p>
-            <h5><a>Password:</a> <input type="password" v-model="password" placeholder="Password"></h5>
+            <h5><a style ="padding:25px;">Email:</a> <input type="text" v-model="email" placeholder="Enter your email here"></h5><p></p>
+            <h5><a style ="padding:5px;">Password:</a> <input type="password" v-model="password" placeholder="Enter you password here"></h5>
             <br>
             <button @click ="login()" id="loginButton">Log-In</button>
         <!--
